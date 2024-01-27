bc3cb2db671dbc9ca921438615afdd121ba6952e
Cass <cassianoleaoramos@gmail.com>
2024-01-27 15:31:21 -0300

    initial commit

---
+++ src/main/java/com/ms/spring_picpay/SpringPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.ms.spring_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPicpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPicpayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ms/spring_picpay/SpringPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.spring_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPicpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
